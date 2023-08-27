de8710a938819c58f9d0b243f57e73afebc19fe6
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2023-08-27 01:54:39 -0700

    Create ComputerChoice function and start on playRound function

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-function getPlayerSelection() {
+function getPlayerChoice() {
     let playerChoice = prompt("Will you choose Rock, Paper, or Scissors? ", "Rock");
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        return console.log(playerChoice);
+        return playerChoice;
     } else {
-        getPlayerSelection();
+        getPlayerChoice();
     }
 }
 
+//Create a function that gets the computer's choice
+//Make sure it's randomly returning either Rock, Paper or Scissors.
 function getComputerChoice() {
+    let choice;
+    let counter = 0;
+    counter = Math.random(counter) * 10;
 
+    if (counter < 3.33) {
+        choice = "rock";
+    } else if(counter < 6.66) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+    return choice;
 }
+
+//Function that plays one round and determines a winner 
+//Include PlayerChoice and ComputerChoice as parameters
+getComputerChoice();
+const computerChoice = getComputerChoice();
+const playerChoice = getPlayerChoice();
+function playRound(playerChoice, computerChoice) {
+    let result;
+    //All choices if player chooses rock
+    if(playerChoice === "rock" && computerChoice === "scissors") {
+        result = "winner";
+    }else if(playerChoice === "rock" && computerChoice === "paper") {
+        result = "loser";
+    }else {
+        result = "tie";
+    }
+    return console.log(result);
+}
+
+console.log(playRound(playerChoice, computerChoice));
+
